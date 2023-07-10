5dead394732a19d4303046ceb806c2162efba9dd
steinskeeper <dpelpsykongroo@gmail.com>
2023-07-10 10:23:08 +0530

    theme

--- agents/marketagent.py
+++ agents/marketagent.py
@@ -6,7 +6,8 @@ from langchain.tools import DuckDuckGoSearchRun
 from langchain.llms import VertexAI
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-
+import vertexai
+from vertexai.language_models import TextGenerationModel
 search = DuckDuckGoSearchRun()
 
 router = APIRouter()
@@ -21,10 +22,17 @@ async def marketAgent_endpoint():
 
 
 async def invoke_market_agent(project_id: str, idea: str):
+    vertexai.init(project="lofty-bolt-383703", location="us-central1")
+    parameters = {
+        "temperature": 0.2,
+        "max_output_tokens": 1000,
+        "top_p": 0.8,
+        "top_k": 40
+    }
     for x in db.hackathons.find():
         technologies = x["technologies"]
         theme = x["theme"]
-    
+
     llm = VertexAI()
     tools = [
         Tool(
@@ -68,8 +76,25 @@ async def invoke_market_agent(project_id: str, idea: str):
         newval = {"question": marketQuestion[i], "answer": agentAnswers[i]}
         final.append(newval)
     print(final)
+
+    model = TextGenerationModel.from_pretrained("text-bison@001")
+    theme_prompt = """
+    Themes : {theme}
+    Idea: {idea}
+    Task : To which of the above themes does the idea belong to?
+    Rules: 
+    1. The theme must be from the above list
+    2. Just give the matched theme and nothing more.
+    3. If the idea does not match any of the themes, just say "None"
+    """
+    theme_prompt = theme_prompt.format(theme=theme, idea=idea)
+    response = model.predict(
+        theme_prompt
+        ** parameters
+    )
+    finalTheme = response.text
     print("project_id", project_id)
     query = {"_id": ObjectId(project_id)}
-    newvalues = {"$set": {"marketAgentAnalysis": final}}
+    newvalues = {"$set": {"marketAgentAnalysis": final, "theme": finalTheme}}
     temp = db.projects.update_one(query, newvalues)
     print("Market Agent : Task Complete")
